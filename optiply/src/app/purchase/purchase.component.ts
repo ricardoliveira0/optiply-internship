922de8ed132f0840889050f2ede94d0fc8cc9968
Ricardo <l42647@alunos.uevora.pt>
2022-03-10 14:53:14 +0000

    add: Table for purchase; 'app.css'
    Add table for purchase. Also add a new file for global styles

--- optiply/src/app/purchase/purchase.component.ts
+++ optiply/src/app/purchase/purchase.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PurchaseData {
+  id: number,
+  orderType: string,
+  supplier: string,
+  noProducts: number,
+  value: number,
+  date: Date
+}
 
 @Component({
   selector: 'optiply-purchase',
   templateUrl: './purchase.component.html',
   styleUrls: ['./purchase.component.css']
 })
-export class PurchaseComponent implements OnInit {
+export class PurchaseComponent implements AfterViewInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'orderType', 'supplier', 'noProducts', 'value', 'date'];
+  dataSource: MatTableDataSource<PurchaseData>;
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() { 
+    const objects = Array.from({length: 20}, (_, k) => this.createNewObject(k + 1));
+    this.dataSource = new MatTableDataSource(objects);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  createNewObject(id: number): PurchaseData {
+    return {
+      id: id,
+      orderType: "Regular",
+      supplier: "Bullet Proof US",
+      noProducts: 7,
+      value: 20484.58,
+      date: new Date()
+    };
   }
 
 }
--- optiply/src/app/purchase/purchase.module.ts
+++ optiply/src/app/purchase/purchase.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PurchaseComponent } from './purchase.component';
 
-
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     PurchaseComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule
   ],
   exports: [
     PurchaseComponent
