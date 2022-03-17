134ccd55729185eac40c556446f98bfb645bf017
Ricardo <l42647@alunos.uevora.pt>
2022-03-17 11:42:52 +0000

    update: Purchase table
    API Callback should now be correctly displayed at the Purchase table. Fix paginator and filter, should now work properly again

--- optiply/src/app/purchase/components/purchase.component.ts
+++ optiply/src/app/purchase/components/purchase.component.ts
@@ -2,7 +2,6 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { Observable, merge, map, startWith, switchMap, catchError, of as observableOf } from 'rxjs';
 
 import { APIService } from '../services';
 import { APICallback } from '../models';
@@ -17,51 +16,38 @@ export class PurchaseComponent implements AfterViewInit, OnInit {
 
   displayedColumns: string[] = ['symbol', 'lastPrice', 'priceChange', 'priceChangePercent', 'highPrice', 'lowPrice'];
   dataSource: MatTableDataSource<any>;
-  data: APICallback[] = [];
-  response: APICallback;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private service: APIService) { }
 
-  /* ngOnInit(): void {
-    this.element_data = this.retrieveData();
-    this.dataSource = new MatTableDataSource();
-  } */
-
-  ngOnInit() { }
-
-  ngAfterViewInit() {
-    //this.dataSource.paginator = this.paginator;
-    //this.dataSource.sort = this.sort;
-
-    merge()
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-          return this.service!.dataCallback().pipe(catchError(() => observableOf(null)));
-        }),
-        map(data => {
-
-          if (data === null) {
-            return [];
-          }
-
-          return data;
-        }),
-      )
-      .subscribe(data => (this.data = data));
+  ngOnInit(): void {
+    this.service.dataCallback()
+    .subscribe(response => {
+      console.log(response);
+      this.dataSource = new MatTableDataSource(response);
+      console.log(this.dataSource);
+      this.dataSource.sort = this.sort;
+    });
   }
 
-  /* applyFilter(event: Event) {
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }, 2000);
+   
+  }
+
+  applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
-  } */
+  }
 
   /* retrieveData(): any {
     return this.service.dataCallback().subscribe(
--- optiply/src/app/purchase/models/api-callback.model.ts
+++ optiply/src/app/purchase/models/api-callback.model.ts
@@ -1,5 +1,10 @@
-export class APICallback {
+export interface APICallback {
 
-    constructor(public symbol: string, public lastPrice: string, public priceChange: string, public priceChangePercent: string, public highPrice: string, public lowPrice: string) {}
-    //[{"symbol":"ETHBTC","priceChange":"-0.00010200","priceChangePercent":"-0.154","lastPrice":"0.06629000","highPrice":"0.06721600","lowPrice":"0.06572100"},
+    symbol: string;
+    lastPrice: string;
+    priceChange: string;
+    priceChangePercent: string;
+    highPrice: string;
+    lowPrice: string;
+    
 }
