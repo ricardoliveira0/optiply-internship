fd82104efc4245ebd09c5051552b5efa8c540a0b
Ricardo <l42647@alunos.uevora.pt>
2022-05-16 02:56:26 +0100

    update: Purchase component

--- optiply/src/app/purchase/components/purchase.component.ts
+++ optiply/src/app/purchase/components/purchase.component.ts
@@ -3,6 +3,7 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { delay } from 'rxjs';
 
 import { APIService } from '../services';
 
